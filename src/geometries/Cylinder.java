24bbaf68c5802f269dcad8cb9bf7673d697534f2
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2024-05-18 23:40:56 +0300

    Before Comments

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,4 +1,13 @@
 package geometries;
+import primitives.Ray;
+import primitives.Point;
+import primitives.Vector;
 
-public class Cylinder {
+public class Cylinder implements Geometry// extends Tube
+{
+    private final double height;
+    public Cylinder(double height) {this.height=height;}
+
+    public Vector getNormal(Point p) {return null;}
+    public Vector getNormal() {return null;}
 }
