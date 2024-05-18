49e4e98b160b2be7b3ff4d406e3c53abd5a183a0
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2024-05-18 23:36:16 +0300

    Before Comments

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,4 +1,21 @@
 package geometries;
 
-public class Cylinder {
+import primitives.Point;
+import primitives.Vector;
+
+public class Cylinder  implements Geometry{
+    final private double height;
+    public Cylinder (double he)
+    {
+        this.height=he;
+    }
+
+
+    public Vector getNormal(Point p) {
+        return null;
+    }
+    public Vector getNormal ()
+    {
+        return null;
+    }
 }
