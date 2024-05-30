aa004d773a9246018baa133a7ea16e72db0145e2
ely <ekupinsk@g.jct.ac.il>
2024-05-30 18:01:21 +0300

    add my

--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -3,8 +3,10 @@ package geometries;
 import org.junit.jupiter.api.Test;
 import primitives.*;
 
+import java.util.Comparator;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SphereTests {
@@ -46,14 +48,33 @@ class SphereTests {
     @Test
     void testFindIntersections()
     {
+
         // ============ Equivalence Partitions Tests ==============
         //TC01: Ray's line is outside the sphere (0 points)
         Sphere sphere = new Sphere(1, p001);
         Ray ray = new Ray(new Point(0,0,3),new Vector(3,0,-3) );
         assertNull(sphere.findIntsersections(ray), "Ray's line out of sphere");
         //TC02: Ray starts before and crosses the sphere (2 points)
-
+        assertEquals(2, result1.size(), "Wrong number of points");
         ray = new Ray(new Point(0,0,3),new Vector(0,0,-1) );
+
+
+        Sphere sphere = new Sphere(p100, 1d);
+        final Point gp1 = new Point(0.0651530771650466, 0.355051025721682, 0);
+        final Point gp2 = new Point(1.53484692283495, 0.844948974278318, 0);
+        final var exp = List.of(gp1, gp2);
+        final Vector v310 = new Vector(3, 1, 0);
+        final Vector v110 = new Vector(1, 1, 0);
+        final Point p01 = new Point(-1, 0, 0);
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Ray's line is outside the sphere (0 points)
+        assertNull(sphere.findIntersections(new Ray(p01, v110)), "Ray's line out of sphere");
+        // TC02: Ray starts before and crosses the sphere (2 points)
+        final var result1 = sphere.findIntersections(new Ray(p01, v310))
+                .stream().sorted(Comparator.comparingDouble(p) -> p.distance(p01))).toList();
+        assertEquals(2, result1.size(), "Wrong number of points");
+        assertEquals(exp, result1, "Ray crosses sphere");
 
 
 
