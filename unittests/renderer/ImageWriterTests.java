d674001503011daf3807b1a3c2b876ca901258eb
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2024-06-16 13:14:37 +0300

    with ImageWriter

---
+++ unittests/renderer/ImageWriterTests.java
@@ -0,0 +1,16 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTests {
+
+    @Test
+    void testWriteToImage() {
+    }
+
+    @Test
+    void testWritePixel() {
+    }
+}
